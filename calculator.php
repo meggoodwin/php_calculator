fbd0954d943553009fbe84e92b6edff23dff4959
meggoodwin <meggoodwin57@gmail.com>
2019-01-24 02:27:05 -0600

    updating calculator code

--- calculator.php
+++ calculator.php
@@ -6,10 +6,15 @@
     <title>PHP Site</title>
 </head>
 <body>
-        
-    <?php
-    echo "Hello Github";
-    ?>
+
+    <form action="site.php" method="get">
+        <input type="number" name="num1">
+        <br>
+        <input type="number" name="num2">
+        <input type="submit">
+    </form>
+
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"]; ?>
 
 </body>
 </html>
